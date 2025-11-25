0ff35f69449b69826730fd2b04720a87d0591c2a
Jônata Marcelino <jonata.marcelino83@gmail.com>
2025-11-25 20:46:40 -0300

    Testando o GitKraken

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import './styles/theme.css';
+import './styles/global.css';
+
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
@@ -7,9 +10,6 @@ import { Cycles } from './components/Cycles';
 import { Button } from './components/Button';
 import { Footer } from './components/Footer';
 import { PlayCircleIcon } from 'lucide-react';
-
-import './styles/theme.css';
-import './styles/global.css';
 
 export function App() {
   return (
