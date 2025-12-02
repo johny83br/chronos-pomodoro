2651ec4e5ef27f526f40da24ebb0933fa939bc09
Jônata Marcelino <jonata.marcelino83@gmail.com>
2025-12-01 23:41:11 -0300

    Colocado a contagem no título da página

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -39,6 +39,9 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
     if (!state.activeTask) {
       worker.terminate();
     }
+
+    document.title = `${state.formatedSecondsRemaining} - Chronos Pomodoro`;
+
     worker.postMessage(state);
   }, [worker, state]);
 
