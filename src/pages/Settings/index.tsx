01ecf42f5a5c0edac7a7d11a4466d4b4b78e117d
Jônata Marcelino <jonata.marcelino83@gmail.com>
2025-12-04 09:37:51 -0300

    Criada a página de configurações

---
+++ src/pages/Settings/index.tsx
@@ -0,0 +1,111 @@
+import { SaveIcon } from 'lucide-react';
+import { Button } from '../../components/Button';
+import { Container } from '../../components/Container';
+import { Heading } from '../../components/Heading';
+import { Input } from '../../components/Input';
+import { MainTemplate } from '../../templates/MainTemplate';
+import { useEffect, useRef } from 'react';
+import { showMessage } from '../../adapters/showMessage';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+
+export function Settings() {
+  const { state } = useTaskContext();
+  const workTimeInput = useRef<HTMLInputElement>(null);
+  const shortBreakTimeInput = useRef<HTMLInputElement>(null);
+  const longBreakTimeInput = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    return () => {
+      showMessage.dismiss();
+    };
+  }, []);
+
+  function handleSaveSettings(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    showMessage.dismiss();
+
+    const formErrors = [];
+
+    const workTime = Number(workTimeInput.current?.value);
+    const shortBreakTime = Number(shortBreakTimeInput.current?.value);
+    const longBreakTime = Number(longBreakTimeInput.current?.value);
+
+    if (isNaN(workTime) || isNaN(shortBreakTime) || isNaN(longBreakTime)) {
+      formErrors.push('Digite apenas números para TODOS os campos');
+    }
+
+    if (workTime < 1 || workTime > 99) {
+      formErrors.push('Digite valores entre 1 e 99 para foco');
+    }
+
+    if (shortBreakTime < 1 || shortBreakTime > 30) {
+      formErrors.push('Digite valores entre 1 e 30 para descanso curto');
+    }
+
+    if (longBreakTime < 1 || longBreakTime > 60) {
+      formErrors.push('Digite valores entre 1 e 60 para descanso longo');
+    }
+
+    if (formErrors.length > 0) {
+      formErrors.forEach(error => {
+        showMessage.error(error);
+      });
+      return;
+    }
+  }
+
+  return (
+    <MainTemplate>
+      <Container>
+        <Heading>Configurações</Heading>
+      </Container>
+
+      <Container>
+        <p style={{ textAlign: 'center' }}>
+          Modifique as configurações para tempo de foco, descanso curto, e
+          descanso longo.
+        </p>
+      </Container>
+
+      <Container>
+        <form onSubmit={handleSaveSettings} action='' className='form'>
+          <div className='formRow'>
+            <Input
+              id='workTime'
+              labelText='Foco'
+              ref={workTimeInput}
+              defaultValue={state.config.workTime}
+              type='number'
+            />
+          </div>
+          <div className='formRow'>
+            <Input
+              id='shortBreakTime'
+              labelText='Descanso curto'
+              ref={shortBreakTimeInput}
+              defaultValue={state.config.shortBreakTime}
+              type='number'
+            />
+          </div>
+          <div className='formRow'>
+            <Input
+              id='longBreakTime'
+              labelText='Descanso longo'
+              ref={longBreakTimeInput}
+              defaultValue={state.config.longBreakTime}
+              type='number'
+            />
+          </div>
+          <div className='formRow'>
+            <Button
+              icon={<SaveIcon />}
+              aria-label='Salvar configurações'
+              title='Salvar configurações'
+            />
+          </div>
+        </form>
+      </Container>
+    </MainTemplate>
+  );
+}
--- src/routers/MainRouter/index.tsx
+++ src/routers/MainRouter/index.tsx
@@ -4,6 +4,7 @@ import { AboutPomodoro } from '../../pages/AboutPomodoro';
 import { NotFound } from '../../pages/NotFound';
 import { useEffect } from 'react';
 import { History } from '../../pages/History';
+import { Settings } from '../../pages/Settings';
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -20,8 +21,9 @@ export function MainRouter() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/history' element={<History />} />
-        <Route path='/about-pomodoro' element={<AboutPomodoro />} />
+        <Route path='/history/' element={<History />} />
+        <Route path='/settings/' element={<Settings />} />
+        <Route path='/about-pomodoro/' element={<AboutPomodoro />} />
 
         <Route path='*' element={<NotFound />} />
       </Routes>
